37db756e5fb61084ab4fe431ed0141fc29662421
Pierre <pierre@pvln.nl>
2017-08-26 09:50:17 +0200

    folder updated

--- site/scripts/archive/cp_srv_rpi2.sh
+++ site/scripts/archive/cp_srv_rpi2.sh
@@ -3,4 +3,4 @@
 
 echo copy archive files from docker server to rpi2
 
-scp -r /var/www/ozitdatzo/*.jpa pi@192.168.2.8:/tmp/archive/
+scp -r /var/www/ozitdatzo/administrator/components/com_akeeba/backup/*.jpa pi@192.168.2.8:/tmp/archive/
